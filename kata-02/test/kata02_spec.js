8d42dcc46d6fc953bb916f130ed5b95f9dd50224
Rob Steward <robert@jukedeck.com>
2017-11-07 09:22:57 +0000

    kata-02: refactor testing to ease adding more algorithms

--- kata-02/test/kata02_spec.js
+++ kata-02/test/kata02_spec.js
@@ -1,11 +1,11 @@
 const assert = require('assert');
 const assert_equal = assert.equal;
 
-const chop = require('../algo-1/bsearch.js');
+const all_algs = [
+	require('../algo-1/bsearch.js')
+];
 
-describe('kata-02', function() {
-	it('should pass the given tests', function() {
-
+const test_algorithm = chop => {
 		assert_equal(-1, chop(3, []))
 		assert_equal(-1, chop(3, [1]))
 		assert_equal(0,  chop(1, [1]))
@@ -27,6 +27,12 @@ describe('kata-02', function() {
 		assert_equal(-1, chop(4, [1, 3, 5, 7]))
 		assert_equal(-1, chop(6, [1, 3, 5, 7]))
 		assert_equal(-1, chop(8, [1, 3, 5, 7]))
+};
 
-	});
+describe('all katas', function() {
+	for (let i = 0; i < all_algs.length; i++) {
+		it(`should pass for algorithm ${i+1}`, () => {
+			test_algorithm(all_algs[i]);
+		});
+	}
 });
